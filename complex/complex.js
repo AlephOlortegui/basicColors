937246bba17c7376075602d35b716e0d870e4caa
NeoDev <you@example.com>
2023-08-04 21:22:14 -0500

    focus, blur listeners, toggle focus class

--- complex/complex.js
+++ complex/complex.js
@@ -2,6 +2,17 @@
 const checkbox = document.getElementById('checkbox');
 const controlForm = document.querySelectorAll('.form-control') // returns a collection
 
+// 2 toggle h4 focus class
+const inputs = document.querySelectorAll('.control-input');
+
+const focusFx = (i) => { i.parentNode.classList.add('focus') }
+const blurFx = (i) => { (!i.value) && i.parentNode.classList.remove('focus') }
+
+inputs.forEach(i => {
+  i.addEventListener('focus', ()=> focusFx(i))
+  i.addEventListener('blur', ()=> blurFx(i))
+})
+
 // 1b: Toggle
 checkbox.addEventListener('click', ()=>{
   controlForm.forEach(i => i.classList.toggle('d-none'))
